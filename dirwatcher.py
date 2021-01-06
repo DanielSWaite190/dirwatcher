af2bea793fde48a30c0869968259c9cfad0cbf81
Daniel S. Waite <danielwaite190@gmail.com>
2021-01-06 15:32:49 -0700

    Adding code from the README file

--- dirwatcher.py
+++ dirwatcher.py
@@ -3,10 +3,13 @@
 Dirwatcher - A long-running program
 """
 
-__author__ = "???"
-
+import logging
+import signal
+import time
 import sys
 
+
+exit_flag = False
 
 def search_for_magic(filename, start_line, magic_string):
     # Your code here
@@ -24,13 +27,44 @@ def create_parser():
 
 
 def signal_handler(sig_num, frame):
-    # Your code here
-    return
+    """
+    This is a handler for SIGTERM and SIGINT. Other signals can be mapped here as well (SIGHUP?)
+    Basically, it just sets a global flag, and main() will exit its loop if the signal is trapped.
+    :param sig_num: The integer signal number that was trapped from the OS.
+    :param frame: Not used
+    :return None
+    """
+    # logger.setLevel(logging.DEBUG)
+
+    # log the associated signal name
+    logger = logging.getLogger(__name__)
+    logger.warn('Received ' + signal.signal(sig_num).name)
 
 
 def main(args):
-    # Your code here
-    return
+    # Hook into these two signals from the OS
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+    # Now my signal_handler will get called if OS sends
+    # either of these to my process.
+
+    while not exit_flag:
+        try:
+            # call my directory watching function
+            pass
+        except Exception as e:
+            # This is an UNHANDLED exception
+            # Log an ERROR level message here
+            print(e)
+            pass
+
+        # put a sleep inside my while loop so I don't peg the cpu usage at 100%
+
+        # time.sleep(polling_interval)
+
+    # final exit point happens here
+    # Log a message that we are shutting down
+    # Include the overall uptime since program start
 
 
 if __name__ == '__main__':
