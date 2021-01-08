e9e8872d1f0410dcacfd446d893311afcbb9ebd6
Daniel S. Waite <danielwaite190@gmail.com>
2021-01-07 18:38:03 -0700

    Working on watch_directory dict.

--- dirwatcher.py
+++ dirwatcher.py
@@ -2,8 +2,8 @@
 """
 Dirwatcher - A long-running program
 """
-
 import argparse
+import datetime
 import logging
 import signal
 import time
@@ -25,9 +25,18 @@ def search_for_magic(filename, start_line, magic_string):
 
 
 def watch_directory(path, magic_string, extension, interval):
-    # Your code here
+    directory_model = {}
+    
+    for director in os.listdir(path):
+        if os.path.isfile(os.path.join(os.path.abspath(path), director)):
+            directory_model [director] = 1
+
+    print(directory_model)
     return
 
+# def scan_single_file():
+# def detect_added_files():
+# def detect_removed_files():
 
 def create_parser():
     """Creates parsing arguments."""
@@ -38,8 +47,8 @@ def create_parser():
         "directory", help="The directory that will be scaned for magic stringt")
     parser.add_argument("magic_string", help="The text that will be searched")
     parser.add_argument(
-        "--polling_interva", "-p", default=.1,
-            help="How many times program refreshes in seconds. Default is .2")
+        "--polling_interva", "-p", default=1,
+            help="How many times program refreshes in seconds. Default is 1")
     parser.add_argument(
         "--file_extension", "-e", default=".txt",
             help="File extension of file to search. Default is .txt")
@@ -49,10 +58,12 @@ def create_parser():
 
 def signal_handler(sig_num, frame):
     global exit_flag
-
+    # logger.warn('Received ' + signal.Signals(sig_num).name)
     # logger.warn('Received ' + signal.signal(sig_num).name)
+    logger.warning(sig_num)
+
     exit_flag = True
-    logger.warning("QUIT")
+    # logger.warning("QUIT")
     return None
 
 
@@ -67,28 +78,40 @@ def main(args):
 
     parsed_args = parser.parse_args(args)
 
+    start_time = datetime.datetime.now()
+    logger.info(
+        "\n" +
+        "-" * 105 +
+        f"\n\tRunning {__file__}" +
+        f"\n\tStarted on {start_time.isoformat()}\n" +
+        "-" * 105
+    )
+
     if not os.path.exists(parsed_args.directory):
         os.mkdir(parsed_args.directory)
 
     while not exit_flag:
-        logger.debug(parsed_args.magic_string)
-        time.sleep(float(parsed_args.polling_interva))
         try:
-            # call my directory watching function
+            watch_directory(parsed_args.directory, 
+                parsed_args.magic_string, parsed_args.file_extension,
+                    parsed_args.polling_interva)
             pass
         except Exception as e:
             # This is an UNHANDLED exception
             # Log an ERROR level message here
             print(e)
             pass
+        
+        time.sleep(float(parsed_args.polling_interva))
 
-        # put a sleep inside my while loop so I don't peg the cpu usage at 100%
-
-        # time.sleep(polling_interval)
-
-    # final exit point happens here
-    # Log a message that we are shutting down
-    # Include the overall uptime since program start
+    up_time = datetime.datetime.now() - start_time
+    logger.info(
+        "\n" +
+        "-" * 105 +
+        f"\n\tStop {__file__}" +
+        f"\n\tUp time was {up_time}\n" +
+        "-" * 105
+    )
 
 
 if __name__ == '__main__':
@@ -98,4 +121,5 @@ if __name__ == '__main__':
     # ---------------------
     # 1. Write all doc strings!
     # 2. gitignor file?
+    # 3. PEP8
     # ---------------------
